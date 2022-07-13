2c1a5d376ebcaa62ce9b8a7db0f185d6e9979a88
Kulmeetcodez <singhkulmeet3@gmail.com>
2022-07-14 02:11:05 +0530

    modified:   src/App.js 	modified:   src/index.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 import React from "react";
 
@@ -8,19 +8,73 @@ function App() {
   return (
     <div className="app">
 
-      <BrowserRouter>
-        <Link>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Link>
-      </BrowserRouter>
+      <Routes>
+
+        <Route path="/">
+          <Home />
+        </Route>
+
+      </Routes>
     </div>
   );
 }
 
 function Home() {
-  return <div className="home">Hello World Again!!</div>;
+  return (
+
+    <div className="home">
+      <h1>Hello World Again!!</h1>
+      <nav>
+        <Link to="/App">App</Link>
+      </nav>
+    </div>
+
+  );
 }
+
+/*
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to React Router!</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
+    </div>
+  );
+}
+
+function Home() {
+  return (
+    <>
+      <main>
+        <h2>Welcome to the homepage!</h2>
+        <p>You can do this, I believe in you.</p>
+      </main>
+      <nav>
+        <Link to="/about">About</Link>
+      </nav>
+    </>
+  );
+}
+
+function About() {
+  return (
+    <>
+      <main>
+        <h2>Who are we?</h2>
+        <p>
+          That feels like an existential question, don't you
+          think?
+        </p>
+      </main>
+      <nav>
+        <Link to="/">Home</Link>
+      </nav>
+    </>
+  );
+}
+*/
 
 export default App;
