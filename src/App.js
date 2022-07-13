348ca3443c1f2ed727d9c5f2f542d53d16ea4d07
Kulmeetcodez <singhkulmeet3@gmail.com>
2022-07-14 02:02:00 +0530

    modified:   src/App.js 	deleted:    src/Manatee.js

--- src/App.js
+++ src/App.js
@@ -2,22 +2,25 @@ import { BrowserRouter, Route, Link } from 'react-router-dom';
 import './App.css';
 import React from "react";
 
-import Manatee from './Manatee';
 
 
 function App() {
   return (
-    <div className="wrapper">
-      <h1>Marine Mammals</h1>
+    <div className="app">
+      <h1>Helo ji</h1>
       <BrowserRouter>
         <Link>
           <Route path="/">
-            <Manatee />
+            <Home />
           </Route>
         </Link>
       </BrowserRouter>
     </div>
   );
+}
+
+function Home() {
+  return <div className="home">Hello World Again!!</div>;
 }
 
 export default App;
