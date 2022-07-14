40e1adf37e8b2c19776e2bfe3e8378d27fc82145
Kulmeetcodez <singhkulmeet3@gmail.com>
2022-07-14 10:21:35 +0530

    modified:   src/Components/Drawer.js

--- src/Components/Drawer.js
+++ src/Components/Drawer.js
@@ -5,14 +5,31 @@ import {
     List,
     ListItem,
     ListItemText,
+    makeStyles
 } from "@material-ui/core";
+
+import IconButton from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
 
 import { Link } from "react-router-dom";
 
+const useStyles = makeStyles(() => ({
+    link: {
+        textDecoration: "none",
+        color: "blue",
+        fontSize: "20px",
+    },
+    icon: {
+        color: "white"
+    }
+}));
+
 function DrawerComponent() {
+    const classes = useStyles();
+    const [openDrawer, setOpenDrawer] = useState(false);
     return (
         <>
-            <Drawer>
+            <Drawer open={openDrawer} onClose={() => setOpenDrawer(false)}>
                 <List>
                     <ListItem>
                         <ListItemText>
@@ -36,7 +53,12 @@ function DrawerComponent() {
                     </ListItem>
                 </List>
             </Drawer>
+            <IconButton onClick={() => setOpenDrawer(!openDrawer)}>
+                <MenuIcon />
+            </IconButton>
         </>
     );
 }
+
+
 export default DrawerComponent;
