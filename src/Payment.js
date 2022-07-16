c6adf8871e9d75cdd204f3d7518a1126ce0cebb0
Kulmeetcodez <singhkulmeet3@gmail.com>
2022-07-16 10:04:27 +0530

    modified:   .firebase/hosting.YnVpbGQ.cache 	modified:   src/Payment.js

--- src/Payment.js
+++ src/Payment.js
@@ -124,24 +124,6 @@ function Payment() {
                 </div>
 
 
-                <div className='payment__section'>
-                    <div className='payment__title'>
-                        <h3>Review items and delivery</h3>
-                    </div>
-                    <div className='payment__items'>
-                        {basket.map(item => (
-                            <CheckoutProduct
-                                id={item.id}
-                                title={item.title}
-                                image={item.image}
-                                price={item.price}
-                                rating={item.rating}
-                            />
-                        ))}
-                    </div>
-                </div>
-
-
                 {/* Payment section - Payment method */}
                 <div className='payment__section'>
                     <div className="payment__title">
